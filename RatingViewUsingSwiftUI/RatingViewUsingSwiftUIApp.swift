c643c4bdbfdb1bd458a27317af104dc584f43016
Gaurav Tak <gaurav.tak@roro.io>
2023-07-30 19:16:18 +0530

    Initial Commit

---
+++ RatingViewUsingSwiftUI/RatingViewUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewUsingSwiftUIApp.swift
+//  RatingViewUsingSwiftUI
+//
+//  Created by Roro Solutions LLP on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
